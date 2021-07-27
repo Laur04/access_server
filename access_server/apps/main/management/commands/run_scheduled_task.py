b14f3f2b910aa9f406b56fe8bab0935d132c60ed
Lauren <2022ldelwich@tjhsst.edu>
2021-07-27 08:16:05 -0400

    add command

---
+++ access_server/apps/main/management/commands/run_scheduled_task.py
@@ -0,0 +1,24 @@
+import os
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from access_server.apps.main.models import ScheduledTask
+
+class Command(BaseCommand):
+    help = "Generate traffic from schedued task."
+
+    def add_arguments(self, parser):
+        parser.add_argument("task_id", type=str, help="ID of scheduled task to run")
+
+    def handle(self, *args, **kwargs):
+        task = ScheduledTask.objects.get(id=int(kwargs['task_id']))
+
+        for h in task.devices:
+            hostname = h.hostname
+            with open(str(settings.MEDIA_ROOT) + '/hosts', 'w') as host_file:
+                host_file.write('[{}]\n{}  ansible_ssh_pass={}\n'.format(hostname, hostname, settings.ANSIBLE_SSH_PASS))
+            os.environ['ACTION_HOST'] = hostname
+
+            for a in task.actions:
+                exec("Runner(['{}'], '{}').run()".format(str(settings.MEDIA_ROOT) + '/hosts', a.script.path))
